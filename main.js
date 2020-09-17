c70270e9fce85595b1ddaba9462ac8176d06a922
evan-kapantais <evankapantais@gmail.com>
2020-09-17 16:49:49 +0300

    add particle.js, decorative sections, modified footer, style cta buttons

---
+++ main.js
@@ -0,0 +1,6 @@
+particlesJS.load('banner', 'particles-banner.json');
+
+particlesJS.load('overview', 'particles.json');
+particlesJS.load('server', 'particles.json');
+particlesJS.load('server-details', 'particles.json');
+particlesJS.load('footer', 'particles-footer.json');
