3e315702abc888b2457d482f653ae08c57452d75
evan-kapantais <evankapantais@gmail.com>
2020-09-18 17:02:37 +0300

    minor changes, add vote page

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ particlesJS.load('overview', 'particles.json');
 particlesJS.load('server', 'particles.json');
 particlesJS.load('server-details', 'particles.json');
 particlesJS.load('footer', 'particles-footer.json');
+particlesJS.load('vote-banner', 'particles-banner.json');
